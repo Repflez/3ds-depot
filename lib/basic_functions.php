d32f91e8dafcad4349a4bdf966b1f904804a1da9
Repflez <repflez@gmail.com>
2016-11-02 22:18:55 -0700

    Add some required backslashes.

--- lib/basic_functions.php
+++ lib/basic_functions.php
@@ -16,13 +16,13 @@ $db = new \Mysqlidb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // CURL DL'r
 function curlDl($path) {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $path);
-	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-	$retValue = curl_exec($ch);
-	curl_close($ch);
+	$ch = \curl_init();
+	\curl_setopt($ch, CURLOPT_URL, $path);
+	\curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+	\curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	\curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	\curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+	$retValue = \curl_exec($ch);
+	\curl_close($ch);
 	return $retValue;
 }
