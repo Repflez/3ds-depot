dfb71dd314d0df5ffa285db71cda39cc81ce3891
Repflez <repflez@gmail.com>
2016-11-02 20:37:34 -0700

    Delete stuff that was before git.
    Blame me for this /shrug.

--- cli.php
+++ cli.php
@@ -64,11 +64,3 @@ function updateDB() {
 	$z->close();
 	exit();
 }
-
-function notify(\cli\Notify $notify, $cycle = 1000000, $sleep = null) {
-	for ($i = 0; $i <= $cycle; $i++) {
-		$notify->tick();
-		if ($sleep) usleep($sleep);
-	}
-	$notify->finish();
-}
